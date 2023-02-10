eaf8d542e39eda0f505e62e03238d32cb41ea6df
paulafuenteg <paula.fuenteg@outlook.es>
2023-02-10 12:54:51 +0100

    Bosque cod

---
+++ Prediccion/mapas_casuales.py
@@ -0,0 +1,5 @@
+mapa_mes = {5:3, 6:3, 7:3, 8:3, 9:3, 4:2, 10:2, 3:1, 11:1, 1:0, 2:0, 12:0}
+mapa_estacion = {"summer": 2, "spring": 2, "autumn":1, "winter":0}
+mapa_dia_semana = {1:1, 2:1, 7:1, 3:0, 4:0, 5:0, 6:0}
+mapa_clima = {3:0, 2:1, 1:2}
+mapa_laborable = {"NO": 0, "SI":1}
---
+++ Prediccion/mapas_registrados.py
@@ -0,0 +1,6 @@
+mapa_mes = {5:2, 6:2, 7:2, 8:2 ,9:2, 10:2,11:1, 12:1, 4:1, 1:0, 2:0, 3:0}
+mapa_estacion = {"summer": 2, "spring":1, "autumn":1, "winter": 0}
+mapa_dia_semana = {4: 2, 5:2, 6:2,3:1, 7:1, 1:0, 2:0}
+mapa_clima = {3:0, 2:1, 1:2}
+mapa_vacaciones = {0:1, 1:0}
+mapa_laborable = {'SI':1, 'NO': 0}
